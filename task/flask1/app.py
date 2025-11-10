220e6187d9b9dab6f08216a9170b42ee63847a22
god-gy <god.gy0321@gmail.com>
2025-11-10 19:28:12 +0900

    flask day1 task

---
+++ task/flask1/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    users = [
+        {"username": "traveler", "name": "Alex"},
+        {"username": "photographer", "name": "Sam"},
+        {"username": "gourmet", "name": "Chris"}
+    ]
+
+    # 1. randering 할 html 파일명 입력
+    # 2. html로 넘겨줄 데이터 입력
+    return render_template('index.html', users=users)
+
+if __name__ == "__main__":
+    app.run(debug=True)
