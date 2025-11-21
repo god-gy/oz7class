e88afbf3234ff5a0faf4f07150323f02a9632b76
god-gy <god.gy0321@gmail.com>
2025-11-21 10:35:50 +0900

    task fastapi_movie

---
+++ task/fastapi_movie/app/shemas/movies.py
@@ -0,0 +1,22 @@
+from typing import Annotated
+from pydantic import BaseModel, Field
+
+class CreateMovieRequest(BaseModel):
+    title: str
+    playtime: int
+    genre: list[str]
+
+class MovieResponse(BaseModel):
+    id: int
+    title: str
+    playtime: int
+    genre: list[str]
+
+class MovieSearchParams(BaseModel):
+    title: str | None = None
+    genre: str | None = None
+
+class MovieUpdateRequest(BaseModel):
+    title: str | None = None
+    genre: list[str] | None = None
+    playtime: Annotated[int, Field(ge=0)]
